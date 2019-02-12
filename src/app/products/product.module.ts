8e25d2017de1672ffa567e7f2bc6106b6741d350
sergey-mukhin <sergey.mukhin@returnonintelligence.com>
2019-02-12 15:41:55 +0300

    initial commit 2.0

---
+++ src/app/products/product.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+
+import { ProductListComponent } from "./product-list.component";
+import { ProductDetailComponent } from "./product-detail.component";
+import { ConvertToSpacesPipe } from "../shared/convert-to-spaces.pipe";
+import { ProductDetailGuard } from "./product-detail.guard";
+import { SharedModule } from "../shared/shared.module";
+
+@NgModule({
+    imports:[
+        SharedModule,
+        RouterModule.forChild([ 
+        { path: 'products', component: ProductListComponent },
+        { path: 'products/:id', component: ProductDetailComponent, canActivate:[ProductDetailGuard] }])
+    ],
+    declarations: [
+        ProductListComponent,
+        ProductDetailComponent,
+        ConvertToSpacesPipe
+    ]
+})
+export class ProductModule { }
